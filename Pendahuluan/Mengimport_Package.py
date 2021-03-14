92731dc962fce125e07a400e14cea9e259fc786b
Ryan <ryanrizkyf@gmail.com>
2021-03-14 14:37:27 +0700

    edit commit

---
+++ Pendahuluan/Mengimport_Package.py
@@ -0,0 +1,8 @@
+# Pertama-tama, kita perlu mengimpor package plotnine terlebih dahulu.
+# Kita masih membutuhkan package pandas untuk membaca data serta melakukan manipulasi data sederhana,
+# sebelum kita membuat visualisasi data yang kita perlukan.
+# Oleh karena itu kita perlu mengimpor package pandas juga:
+
+import matplotlib.pyplot as plt
+import plotnine as p9
+import pandas as pd
